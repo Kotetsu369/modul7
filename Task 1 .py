86e18780ca26f55c9b3c845141b898da4be5c48e
Kotetsu369 <christi-95@mail.ru>
2024-01-14 22:27:02 +0900

    in process

--- Task 1 .py
+++ Task 1 .py
@@ -6,9 +6,18 @@ url = 'https://mfd.ru/currency/?currency=USD&from='
 page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 ''Firefox/50.0'})
 
 soup = BeautifulSoup(page.text, 'html.parser')
-table = soup.findAll('table', {'class': "mfd-table mfd-currency-table"})
+table = soup.find_all('table', {'class': "mfd-table mfd-currency-table"})
 
-print([itog.text for itog in table])
+table1 = [itog.text for itog in table]
+
+string = ''
+for el in table1:
+    string += str(el) #Превращаем каждый элемент списка в строку
+string.replace('\n', '')
+
+
+print(string)
+#print([itog.text for itog in table1])
 
 
 
